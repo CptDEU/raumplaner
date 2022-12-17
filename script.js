d8ea85650ceea032ac328d93b898a6a8dcebb4c8
CptDEU~ <mustafa@soenmezoglu.de>
2022-12-17 11:33:37 +0100

    js änderungen

--- script.js
+++ script.js
@@ -5,13 +5,14 @@ let cloneElement = false;
 
 function startDrag(e, clone) {
   const target = e.target;
-
-  if (target.tagName === "IMG" || target.id === "resizeMe") {
+  
+  if (target.parentNode.classList.contains("resize-image") || target.id === "resizeMe") {
     x = e.offsetX;
     y = e.offsetY;
-    currentTarget = target;
+    currentTarget = target.parentNode.classList.contains("resize-image") ? target.parentNode : target;
     cloneElement = clone;
   }
+  selectedItem();
 }
 
 function cloneImage(e) {
@@ -22,23 +23,26 @@ function moveImage(e) {
   startDrag(e, false);
 }
 
-function removeImage(e) {
+function removeImage(e) { 
   if (!cloneElement) {
+    currentTarget.remove("inside");
     currentTarget.remove();
   }
 }
 
-function stickImage(e) {
+function stickImage(e) { 
   const image = cloneElement ? currentTarget.cloneNode(true) : currentTarget;
-
+  image.classList.add("inside");
+  
   imagesCanvasElement.appendChild(image);
-
+  
   // + 1 for the border
-
-  image.style.left = e.pageX - imagesCanvasElement.offsetLeft - x + 1 + "px";
-  image.style.top = e.pageY - imagesCanvasElement.offsetTop - y + 1 + "px";
-
+  
+  image.style.left = (e.pageX - imagesCanvasElement.offsetLeft - x + 1) + 'px';
+  image.style.top = (e.pageY - imagesCanvasElement.offsetTop - y + 1) + 'px';
+  
   currentTarget = null;
+  selectedItem();
 }
 
 function allowDrag(e) {
@@ -46,32 +50,32 @@ function allowDrag(e) {
 }
 
 // Bind event listeners:
-const imagesBarElement = document.getElementById("imagesBar");
-const imagesCanvasElement = document.getElementById("raum-bearbeitung");
+const imagesBarElement = document.getElementById('imagesBar');
+const imagesCanvasElement = document.getElementById('raum-bearbeitung');
 const raumElemnent = document.querySelector("#resizeMe");
 
-document.addEventListener("dragenter", allowDrag);
-document.addEventListener("dragover", allowDrag);
+document.addEventListener('dragenter', allowDrag);
+document.addEventListener('dragover', allowDrag);
 
-imagesBarElement.addEventListener("dragstart", cloneImage);
-imagesBarElement.addEventListener("drop", removeImage);
+imagesBarElement.addEventListener('dragstart', cloneImage);
+imagesBarElement.addEventListener('drop', removeImage);
 
-raumElemnent.addEventListener("dragstart", cloneImage);
-raumElemnent.addEventListener("drop", removeImage);
+raumElemnent.addEventListener('dragstart', cloneImage);
+raumElemnent.addEventListener('drop', removeImage);
 
-imagesCanvasElement.addEventListener("dragstart", moveImage);
-imagesCanvasElement.addEventListener("drop", stickImage);
+imagesCanvasElement.addEventListener('dragstart', moveImage);
+imagesCanvasElement.addEventListener('drop', stickImage);
 
 // Resizeable
 const resizeableRaum = document.querySelector(".resizable");
-const raumWidth = document.querySelector("#raum-width");
-const raumHeight = document.querySelector("#raum-height");
+const raumWidth = document.querySelector(".raum-width");
+const raumHeight = document.querySelector(".raum-height");
 
 new ResizeObserver((e) => {
   const height = e[0].target.clientHeight;
   const width = e[0].target.clientWidth;
-  raumWidth.innerHTML = `${(Number(width) / 50).toFixed(1)}m`;
-  raumHeight.innerHTML = `${(Number(height) / 50).toFixed(1)}m`;
+  raumWidth.innerHTML = `${(Number(width)/50).toFixed(1)}m`;
+  raumHeight.innerHTML = `${(Number(height)/50).toFixed(1)}m`;
 }).observe(raumElemnent);
 
 // Select Texture
@@ -79,24 +83,26 @@ const textures = {
   white: "white",
   black: "black",
   holz: "url(https://u7.uidownload.com/vector/220/493/vector-wood-texture-eps-svg.jpg)",
-  marmor:
-    "url(https://u7.uidownload.com/vector/296/547/vector-grey-marble-texture-background-vector-free-eps-ai.jpg)",
-};
+  marmor: "url(https://u7.uidownload.com/vector/296/547/vector-grey-marble-texture-background-vector-free-eps-ai.jpg)"
+}
 
 const texturSelector = document.querySelector("#textur");
 texturSelector.addEventListener("change", (e) => {
   resizeableRaum.style.background = textures[e.target.value];
-});
+})
 
 // Resize mit Input Felder
 const breiteInput = document.querySelector("#breite");
 const hoeheInput = document.querySelector("#hoehe");
 const formElement = document.querySelector(".dauerhafte-werte");
+const moebelWerte = document.querySelector(".variable-werte");
+const modifier = 1;
+let currentItem = null;
 
 formElement.addEventListener("submit", (e) => {
   e.preventDefault();
-  raumWidth.innerHTML = `${Number(breiteInput.value).toFixed(1)}m`;
-  raumHeight.innerHTML = `${Number(hoeheInput.value).toFixed(1)}m`;
+  raumWidth.innerHTML = `${(Number(breiteInput.value)).toFixed(1)}m`;
+  raumHeight.innerHTML = `${(Number(hoeheInput.value)).toFixed(1)}m`;
 
   // Change width & height
   raumElemnent.style.width = `${Number(breiteInput.value) * 50}px`;
@@ -105,4 +111,64 @@ formElement.addEventListener("submit", (e) => {
   // Reset Input Fields
   breiteInput.innerHTML = "";
   hoeheInput.innerHTML = "";
-});
+})
+moebelWerte.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log(currentItem)
+  // raumWidth.innerHTML = `${(Number(breiteInput.value)).toFixed(1)}m`;
+  // raumHeight.innerHTML = `${(Number(hoeheInput.value)).toFixed(1)}m`;
+
+  // // Change width & height
+  // raumElemnent.style.width = `${Number(breiteInput.value) * 50}px`;
+  // raumElemnent.style.height = `${Number(hoeheInput.value) * 50}px`;
+
+  // // Reset Input Fields
+  // breiteInput.innerHTML = "";
+  // hoeheInput.innerHTML = "";
+})
+
+function handleKeyMovent(event) {
+  const { style } = currentItem;
+  switch(event.key) {
+    case "ArrowUp": style.top = `${parseInt(style.top) - modifier}px`; break;
+    case "ArrowDown": style.top = `${parseInt(style.top) + modifier}px`; break;
+    case "ArrowLeft": style.left = `${parseInt(style.left) - modifier}px`; break;
+    case "ArrowRight": style.left = `${parseInt(style.left) + modifier}px`; break;
+  }
+}
+
+function selectedItem() {
+  const parent = imagesCanvasElement;
+  const children = parent.children;
+  const allCurrentItems = document.querySelectorAll(".current");
+
+  // Reset
+  for(let item of allCurrentItems) {
+    if(item.classList.contains("current")) {
+      item.classList.remove("current");
+    }
+  }
+  moebelWerte.style.display = "none";
+  currentItem = null;
+  window.removeEventListener("keydown", handleKeyMovent);
+
+  [...children].forEach(child => {
+    child.addEventListener("click", () => {
+      currentItem = null;
+      if(!child.classList.contains("resizeMe")) {
+        currentItem = child;
+        child.classList.add("current");
+
+        // Bewege Möbel mit dem Pfeilen auf der Tastatur
+        window.addEventListener("keydown", handleKeyMovent)
+
+        if(currentItem) {
+          moebelWerte.style.display = "flex";
+        }
+      }
+    })
+  });
+
+  
+
+}
